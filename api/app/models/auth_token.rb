e1d05be6495efcf84fa0e6d0bafee743d6b4b779
Mark Brouch <mark.brouch@gmail.com>
2020-11-08 20:32:44 -1000

    ✏️ Reorder definitions

--- api/app/models/auth_token.rb
+++ api/app/models/auth_token.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 class AuthToken < ApplicationRecord
+  belongs_to :user
+  validates_presence_of :user
+
   def self.find_by_jwt(jwt)
     decoded_token = JsonWebToken.decode_token(jwt)
     return unless decoded_token
 
     find_by_id(decoded_token[0]['id'])
   end
-
-  belongs_to :user
-  validates_presence_of :user
 
   def to_jwt
     JsonWebToken.encode_token(id: id)
